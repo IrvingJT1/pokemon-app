4e834fc0ffe04c0f5366aec18077f670041e6414
IrvingJT1 <irvinjt@gmail.com>
2025-07-27 12:18:03 -0600

    Update
    -Deleting unused dependencies
    -Fixing details screen spacing

--- src/components/PokeDetails.tsx
+++ src/components/PokeDetails.tsx
@@ -7,13 +7,18 @@ export const PokeDetails = () => {
     const location = useLocation();
     const {pokemon} = location.state || {};
     
+    //Se usa inline style debido a la limitacion de bootstrap con el parámetro top
+    const top = {
+    top: "55%"
+    };
 
     return (
 
         <div className="container">
             <h1>PokemonApp</h1>
             <hr />
-            <div className="card bg-dark text-white border-white w-25 my-5 position-absolute top-50 start-50 translate-middle">
+            
+                <div className="card bg-dark text-white border-white w-25 mt-5 position-absolute start-50 translate-middle" style={top}>
                 <img src={pokemon.photo? pokemon.photo : noImage } className="card-img-top" alt={pokemon.name}/>
                 <div className="card-body">
                     <h5 className="card-title">{pokemon.name.toUpperCase()}</h5>
@@ -28,7 +33,8 @@ export const PokeDetails = () => {
                     <li className="list-group-item bg-dark text-white border-white">Altura: {pokemon.height * 10} cm</li>
                     <li className="list-group-item bg-dark text-white border-white">Peso: {pokemon.weight} lbs.</li>
                 </ul>
-            </div>
+                </div>
+            
         </div>
 
         
